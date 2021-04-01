87ec35e01a1fbda1bc0c4c130480bb0a3aa76601
Durapensa <durapensa@gmail.com>
2021-04-01 14:25:26 -0400

    ppdd pip installed, tested & working

--- pico_pi_controller/_nbdev.py
+++ pico_pi_controller/_nbdev.py
@@ -2,18 +2,20 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"PwrLedFlicker": "00_core.ipynb",
-         "REG_NAME": "00_core.ipynb",
-         "empty": "00_core.ipynb",
-         "pause": "00_core.ipynb",
-         "hostname": "00_core.ipynb",
-         "bosmang": "00_core.ipynb",
-         "cmd_code": "00_core.ipynb",
-         "i2c_event_handler": "50_daemon.ipynb",
-         "main": "50_daemon.ipynb"}
+index = {"logger": "50_ppdd.ipynb",
+         "log_txn": "50_ppdd.ipynb",
+         "PwrLedFlicker": "50_ppdd.ipynb",
+         "i2c_event_handler": "50_ppdd.ipynb",
+         "main": "50_ppdd.ipynb",
+         "empty": "50_ppdd.ipynb",
+         "pause": "50_ppdd.ipynb",
+         "hostname": "50_ppdd.ipynb",
+         "cmd_code": "50_ppdd.ipynb",
+         "REG_NAME": "50_ppdd.ipynb",
+         "REG_INT": "50_ppdd.ipynb"}
 
 modules = ["core.py",
-           "daemon.py"]
+           "ppdd.py"]
 
 doc_url = "https://durapensa.github.io/pico_pi_controller/"
 
