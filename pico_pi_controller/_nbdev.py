2262ec48c732499cd97b577b9ccfde7404302463
Durapensa <durapensa@gmail.com>
2021-04-03 22:15:26 -0400

    started ppcc command line utility

--- pico_pi_controller/_nbdev.py
+++ pico_pi_controller/_nbdev.py
@@ -2,21 +2,26 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"logger": "50_ppdd.ipynb",
-         "log_txn": "50_ppdd.ipynb",
+index = {"logger": "60_ppcc.ipynb",
+         "log_txn": "60_ppcc.ipynb",
          "PwrLedFlicker": "50_ppdd.ipynb",
+         "PPCcHandler": "50_ppdd.ipynb",
+         "goodbye": "60_ppcc.ipynb",
          "i2c_event_handler": "50_ppdd.ipynb",
-         "main": "50_ppdd.ipynb",
+         "main": "60_ppcc.ipynb",
          "empty": "50_ppdd.ipynb",
          "pause": "50_ppdd.ipynb",
          "hostname": "50_ppdd.ipynb",
          "bosmang": "50_ppdd.ipynb",
          "cmd_code": "50_ppdd.ipynb",
-         "REG_NAME": "50_ppdd.ipynb",
-         "REG_INT": "50_ppdd.ipynb"}
+         "tcp_address": "60_ppcc.ipynb",
+         "REG_NAME": "60_ppcc.ipynb",
+         "REG_INT": "60_ppcc.ipynb",
+         "connect_ppdd": "60_ppcc.ipynb"}
 
 modules = ["core.py",
-           "ppdd.py"]
+           "ppdd.py",
+           "ppcc.py"]
 
 doc_url = "https://durapensa.github.io/pico_pi_controller/"
 
